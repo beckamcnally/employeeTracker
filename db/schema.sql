38f0213192653820ff60a84b5b071f6b9b93b791
Becka McNally <beckamcnally@gmail.com>
2023-02-07 19:56:59 -0800

    server file

--- db/schema.sql
+++ db/schema.sql
@@ -18,12 +18,13 @@ CREATE TABLE role (
   REFERENCES department(id)
   ON DELETE SET NULL
 );
+
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
   first_name VARCHAR(30)
   last_name VARCHAR(30)
   role_id INT NOT NULL
-  manager_id INT NOT NULL
+  manager_id INT 
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   ON DELETE SET NULL
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mysql = require('mysql2');
+const api = require('./routes/index')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use('/api', api);
+
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}/`);
+});
